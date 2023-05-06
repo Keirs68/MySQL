38a5dd586c06ec4cb9feea6054b0cd5cd6cbf921
Keirs68 <putrimaulida1070@gmail.com>
2023-05-06 22:46:46 +0700

    Add Tugas 8

---
+++ Tugas 8.sql
@@ -0,0 +1,11 @@
+-- Detail Tugas
+Buatlah bisnis proses pembayaran dengan menggunakan trigers, dengan skenario sebagai berikut :
+
+1. pelanggan memesan didalam table pesanan
+
+2. dilanjutkan dengan proses pembayaran di table pembayara
+
+3. didalam table pembayaran tambahkan kolom status_pembayaran
+
+4. jika pesanan sudah dibayar maka status pembayaran akan berubah menjadi lunas 
+
